e89edf5baae2fd239b9c2bf3c78283e94c14dab6
Masked <trist13579@gmail.com>
2024-09-02 23:50:36 -0400

    lol package builder page and readme update

--- README.md
+++ README.md
@@ -1,4 +1,4 @@
-# Website utility built for US Navy sailors
+# Website utility built for US Navy CWTs (CTNs)
 
 # Tech Stach
 - Typescript, Vue 3, Nuxt 3, NuxtUI, Supabase (auth & database)
---
+++ pages/packagebuilder.vue
@@ -0,0 +1,14 @@
+<template>
+  <UCard class="rounded-2xl ml-[10%] mr-[10%] mt-[10%] min-h-96">
+    <div class="flex flex-col place-items-center">
+      <h1 class="text-4xl font-bold m-1 mb-5">Package Builder</h1>
+    </div>
+  </UCard>
+</template>
+
+<script lang="ts" setup>
+</script>
+
+<style>
+
+</style>
